7fa5e8edb3d5dada99b2e111545eb60816615a19
Andrey <andrey.udeneew@mail.ru>
2022-01-30 22:01:24 +0300

    v0.2 adapthisteq was added

--- boerdesDetection.m
+++ boerdesDetection.m
@@ -6,14 +6,18 @@ for i=1:length(list)
     list(i).IM=imread(list(i).name);
     list(i).IMgray=rgb2gray(list(i).IM);
     list(i).adjusted=imadjust(list(i).IMgray);
+    list(i).adapted=adapthisteq(list(i).IMgray);
     imAdjusted(i).name=['ADJUSTED',list(i).name];
+%     [list(i).Gx,list(i).Gy] = imgradientxy(list(i).adjusted);
+%     [list(i).Gmag,list(i).Gdir] = imgradient(list(i).Gx,list(i).Gy);
+%     list(i).edge = edge(list(i).IMgray);
+%     list(i).Gmag=imadjust(list(i).Gmag);
 
     fig1=gcf;
     fig1.FileName=imAdjusted(i).name;
     fig1.Name=imAdjusted(i).name;
     title('imAdjusted(i).name');
-    imshow(list(i).adjusted);
-%     quiver(list(i).Gx,list(i).Gy);
+    imshow([list(i).IMgray,list(i).adjusted,list(i).adapted]);
     saveas(gcf,fig1.FileName);
     clf;
 end
